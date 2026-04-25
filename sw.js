7dd8e20e6bb2e24cb3d3aba6ad46b3c3fa86c9ab
mateusbalbinovieira-maker <mateusbalbinovieira@gmail.com>
2026-04-25 19:20:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
-var CACHE = 'aeset-vr-v1';
-var FILES = ['/', '/index.html'];
+var CACHE = 'aeset-vr-v2';
+var FILES = [
+  '/aeset-vr/',
+  '/aeset-vr/index.html',
+  '/aeset-vr/manifest.json',
+  '/aeset-vr/icon-192.png',
+  '/aeset-vr/icon-512.png'
+];
 
 self.addEventListener('install', function(e){
   e.waitUntil(
-    caches.open(CACHE).then(function(c){ return c.addAll(FILES); })
+    caches.open(CACHE).then(function(c){ return c.addAll(FILES); }).catch(function(){})
   );
   self.skipWaiting();
 });
@@ -19,6 +25,10 @@ self.addEventListener('activate', function(e){
 
 self.addEventListener('fetch', function(e){
   e.respondWith(
-    caches.match(e.request).then(function(r){ return r || fetch(e.request); })
+    caches.match(e.request).then(function(r){
+      return r || fetch(e.request).catch(function(){
+        return caches.match('/aeset-vr/index.html');
+      });
+    })
   );
 });
